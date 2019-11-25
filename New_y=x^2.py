5a5394c1794481c237ab0f09c16dda6c494bfecc
Nguyen Nguyen <nguyen@Nguyens-MacBook-Pro.local>
2019-11-25 15:06:04 -0500

    Updated Relu

--- New_y=x^2.py
+++ New_y=x^2.py
@@ -2,8 +2,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-XAll = np.random.rand(100, 1) * 20 - 10
-yAll = np.abs(XAll)   #** 2 #+ 0.1*np.random.randn(100, 1) # noise added
+XAll = np.random.rand(100, 1) * 2 - 1
+yAll = np.abs(XAll) # noise added
 train_ratio = 0.85
 num_train_datum = int(train_ratio * 100)
 X = XAll[0:num_train_datum]  # training data
@@ -12,14 +12,14 @@ y = yAll[0:num_train_datum]
 y_test = yAll[num_train_datum:]
 
 class Neural_Network(object):
-  def __init__(self):
+  def __init__(self, activation="sigmoid"):
   #parameters
     self.inputSize = 1
     self.outputSize = 1
-    self.hiddenSize = 300
+    self.hiddenSize = 5
   #weights
-    self.W1 = np.random.randn(self.inputSize,  self.hiddenSize)     # weight matrix from input to hidden layer
-    self.W2 = np.random.randn(self.hiddenSize, self.outputSize)     # weight matrix from hidden to output layer
+    self.W1 = np.random.randn(self.inputSize,  self.hiddenSize) * 0.01    # weight matrix from input to hidden layer
+    self.W2 = np.random.randn(self.hiddenSize, self.outputSize)  * 0.01  # weight matrix from hidden to output layer
 
     self.b1 = np.zeros((1, self.hiddenSize))
     self.b2 = np.zeros((1, self.outputSize))
@@ -27,6 +27,13 @@ class Neural_Network(object):
     self.z2 = None
     self.z3 = None
     self.o  = None
+    self.activation = activation
+    if activation =="sigmoid":
+        self.z_activation= self.sigmoid
+        self.Prime = self.sigmoidPrime
+    else:
+        self.z_activation= self.relu
+        self.Prime = self.reluPrime
 
   def sigmoid(self, s):
     # activation function
@@ -35,6 +42,14 @@ class Neural_Network(object):
   def sigmoidPrime(self, s):
     #derivative of sigmoid
     return self.sigmoid(s) * (1 - self.sigmoid(s))
+
+  def relu(self, s):
+      return np.maximum(0, s)
+
+  def reluPrime(self, s):
+      s[s <= 0] = 0
+      s[s > 0] = 1
+      return s
 
   def cost(self, X, y):
       output = self.forward(X)
@@ -53,19 +68,22 @@ class Neural_Network(object):
 
       return np.sum(curLayerError, axis=0, keepdims=True)
 
-
-  def forward(self, X):
+  def forward(self, X, activation="sigmoid"):
     #forward propagation through our network
     self.z = np.dot(X, self.W1) + self.b1           # dot product of X (input) and first set of 3x2 weights
-    self.z2 = self.sigmoid(self.z)                  # activation function
+    if activation == "sigmoid":
+        self.z2 = self.z_activation(self.z)
+
+        # activation function
     self.z3 = np.dot(self.z2, self.W2) + self.b2    # dot product of hidden layer (z2) and second set of 3x1 weights
     self.o = self.z3                                # final activation function
     return self.o
 
   def backward(self, X, y, output):
     # backward propagate through the network
-    self.o_error  = self.costGradient(output, y) * self.sigmoidPrime(self.z3)
-    self.z2_error = np.dot(self.o_error, self.W2.T) * self.sigmoidPrime(self.z)
+
+    self.o_error  = self.costGradient(output, y) * self.Prime(self.z3)
+    self.z2_error = np.dot(self.o_error, self.W2.T) * self.Prime(self.z)
 
     # Weight Gradient
     self.z2_delta = self.dCdW(X, self.z2_error)
@@ -90,7 +108,7 @@ class Neural_Network(object):
 
 
   def train(self, X, y):
-      self.output = self.update_weights(X, y, lr=0.001)
+      self.output = self.update_weights(X, y, lr=0.0005)
 
   def saveWeights(self):
     np.savetxt("w1.txt", self.W1, fmt="%s")
@@ -101,18 +119,18 @@ class Neural_Network(object):
     print ("Input (scaled): \n" + str(X_test));
     print("Output: \n" + str(self.forward(X_test)))
 
-NN = Neural_Network()
-for i in range(1000): # trains the NN 1,000 times
-  print ("# " + str(i) + "\n")
-  print ("Input (scaled): \n" + str(X))
-  print ("Actual Output: \n" + str(y))
-  print ("Predicted Output: \n" + str(NN.forward(X)))
-  print("Loss: \n" + str(np.mean(np.square(y - NN.forward(X)))))  # mean sum squared loss
-  print ("\n")
+NN = Neural_Network(activation="relu")
+for i in range(5000): # trains the NN 1,000 times
+  #print ("# " + str(i) + "\n")
+  # print ("Input (scaled): \n" + str(X))
+  # print ("Actual Output: \n" + str(y))
+  # print ("Predicted Output: \n" + str(NN.forward(X)))
+  print("Loss:" + str(np.mean(np.square(y - NN.forward(X)))))  # mean sum squared loss
+  #print ("\n")
   NN.train(X, y)
 
 NN.saveWeights()
-NN.predict()
+#NN.predict()
 output = NN.forward(X)
 plt.plot(X, y, "ro")
 plt.plot(X,output, "bo")
